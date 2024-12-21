098f83d3732ae6a746c0dbac21896bb7d42b181d
DGSB_ <tzotimovforwork@gmail.com>
2024-12-21 22:49:26 +0300

    Added add_new_task prototype

--- task.py
+++ task.py
@@ -1,15 +1,49 @@
-class Task:
-    def __init__(self,completed=False,content="",section=0,due_date="",should_repeat=""):
-        self.content = content
-        self.section = section
-        self.due_date = due_date
-        self.should_repeat = should_repeat
-        self.completed = completed
+from optparse import Option
 
-    def new(self,completed,content,section,due_date,should_repeat):
+
+class Task:
+    def __init__(self,id=0, completed=False,content="",section=0,due_date="",should_repeat=""):
+        self.id = id
         self.content = content
         self.section = section
         self.due_date = due_date
         self.should_repeat = should_repeat
         self.completed = completed
-        return self
+    def complete(self):
+        self.completed = True
+
+class TaskManager:
+    def __init__(self, tasks=[],sections=[],next_id=0, removed_ids=[]):
+        self.tasks=tasks
+        self.sections=sections
+        self.next_id=next_id
+        self.removed_ids = removed_ids
+
+    def add_task(self,completed,content,section,due_date,should_repeat):
+        self.tasks.append(Task(completed,content,section,due_date,should_repeat))
+
+    def remove_task(self, id):
+        self.removed_ids.append(id)
+        for task in self.tasks:
+            if task.id == id:
+                self.tasks.remove(task)
+
+    def get_task(self, id):
+        for task in self.tasks:
+            if task.id == id:
+                return task
+    def get_tasks_by_section(self, section):
+        return [task for task in self.tasks if task.section == section]
+
+    def get_tasks(self):
+        return self.tasks
+
+    def edit_task(self, id, content, section, due_date, should_repeat):
+        for task in self.tasks:
+            if task.id == id:
+                task.content = content
+                task.section = section
+                task.due_date = due_date
+                task.should_repeat = should_repeat
+
+
